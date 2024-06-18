558566576ba6fd07b18972667023313d6ed7181f
Jacob Valdez <jacobfv123@gmail.com>
2024-06-18 11:02:31 -0500

    back to typeorm

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,12 +1,34 @@
-import drizzle from "drizzle-orm";
+import { config } from "@/core/config";
+import { singleton } from "@/utils/singleton-runner";
+import { DataSource, EntitySchema } from "typeorm";
 
-const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "social_media_network",
-};
+let appDataSource: DataSource;
+const entities: (Function | EntitySchema)[] = [];
 
-const dbConnection = drizzle.createConnection(dbConfig);
+const ensureDataSourceInitialized = singleton(async () => {
+  if (appDataSource) {
+    console.debug("Data source already initialized!");
+    return;
+  }
+  console.debug("Initializing data source...");
+  appDataSource = new DataSource({
+    type: "postgres",
+    host: config.databaseHost,
+    port: config.databasePort,
+    username: config.databaseUser,
+    password: config.databasePassword,
+    database: config.databaseName,
+    entities: entities,
+  });
+  console.debug("Data source has been initialized!");
+});
 
-export default db;
+export async function getDataSource() {
+  await ensureDataSourceInitialized();
+  return appDataSource;
+}
+
+export function registerEntity(entity: Function | EntitySchema) {
+  entities.push(entity);
+  return entity;
+}
